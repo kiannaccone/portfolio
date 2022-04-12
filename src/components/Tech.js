f5128af4d15eceb7423bb436730cf9b048204e65
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2022-04-11 18:32:04 -0600

    tech icons are all working

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -2,16 +2,17 @@
 function Tech() {
     return (
       <div className="flex-row flex-wrap" id="tech-stack-wrapper">
-          <a href="https://www.ruby-lang.org/"><img id="tech-icon1" className="tech-icon" src="/images/ruby.png" alt="Ruby icon" title="Ruby"/></a>
-          <a href="https://rubyonrails.org/"><img id="tech-icon2" className="tech-icon" src="/images/rails.png" alt="Rails icon" title="Rails"/></a>
-          <a href="https://reactjs.org/"><img id="tech-icon3" className="tech-icon" src="images/react.png" alt="React icon" title="React"/></a>
-          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"><img id="tech-icon4" className="tech-icon" src="/images/javascript.png" alt="JavaScript icon" title="JavaScript"/></a>
-          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS"><img id="tech-icon5" className="tech-icon" src="/images/css.jpg" alt="CSS icon" title="CSS"/></a>
-          <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML"><img id="tech-icon6" className="tech-icon" src="/images/html.png" alt="HTML icon" title="HTML"/></a>
-          <a href="https://www.heroku.com/"><img id="tech-icon8" className="tech-icon" src="/images/heroku.webp" alt="Heroku icon" title="Heroku"/></a>
-          <a href="https://github.com/"><img id="tech-icon11" className="tech-icon" src="/images/github.png" alt="Github icon" title="Github"/></a>
+          <a href="https://www.ruby-lang.org/" target="_blank"><img id="tech-icon1" className="tech-icon" src="/images/ruby.png" alt="Ruby icon" title="Ruby"/></a>
+          <a href="https://rubyonrails.org/" target="_blank"><img id="tech-icon2" className="tech-icon" src="/images/rails.png" alt="Rails icon" title="Rails"/></a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img id="tech-icon3" className="tech-icon" src="/images/javascript.png" alt="JavaScript icon" title="JavaScript"/></a>
+          <a href="https://reactjs.org/" target="_blank"><img id="tech-icon4" className="tech-icon" src="images/react.png" alt="React icon" title="React"/></a>
+          <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><img id="tech-icon5" className="tech-icon" src="/images/css.png" alt="CSS icon" title="CSS"/></a>
+          <a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML" target="_blank"><img id="tech-icon6" className="tech-icon" src="/images/html.png" alt="HTML icon" title="HTML"/></a>
+          <a href="https://www.heroku.com/" target="_blank"><img id="tech-icon7" className="tech-icon" src="/images/heroku.webp" alt="Heroku icon" title="Heroku"/></a>
+          <a href="https://github.com/" target="_blank"><img id="tech-icon8" className="tech-icon" src="/images/github.png" alt="Github icon" title="Github"/></a>
+          <a href="https://git-scm.com/" target="_blank"><img id="tech-icon9" className="tech-icon" src="/images/git.png" alt="Git icon" title="Git"/></a>
       </div>
     );
-  }
+}
   
-  export default Tech;
+export default Tech;
